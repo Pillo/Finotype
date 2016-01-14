b66d2c51358433acfc20c5e3bc23fe472e4b2c65
Taylor <tcourtney@pillocompany.com>
2016-01-13 22:16:07 -0500

    Added Code for Removing Background

--- app/src/main/java/finotype/pillo/com/finotype/MainActivity.java
+++ app/src/main/java/finotype/pillo/com/finotype/MainActivity.java
@@ -1,15 +1,26 @@
 package finotype.pillo.com.finotype;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int SELECT_PHOTO = 1;
+    ImageView imageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +28,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        imageView= (ImageView) findViewById(R.id.imageView);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, SELECT_PHOTO);
             }
         });
     }
@@ -34,6 +47,106 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    public void backgroundColor(Bitmap bitmap){
+
+
+        Bitmap bitmap1= Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.RGB_565);
+        int intArray[] = new int[bitmap.getWidth()*bitmap.getHeight()];
+        bitmap.getPixels(intArray, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+
+        int top=0;
+        int bottom=0;
+        int right=0;
+        int left=0;
+        int count=0;
+        int x=0;
+        int y=0;
+        int w=0;
+
+
+        for(int j=0; j<bitmap.getWidth(); j++){
+            count++;
+            top+=intArray[j];
+        }
+
+        top= (top/count);
+        if (top<0)
+            top= top*-1;
+        count=0;
+        for(int j=intArray.length-bitmap.getWidth(); j<bitmap.getWidth()*bitmap.getHeight(); j++){
+            count++;
+            bottom+=intArray[j];
+        }
+        if (count!=0)
+        bottom=(bottom/count);
+        if (bottom<0)
+            bottom= bottom*-1;
+        count=0;
+        for(int j=0; j<bitmap.getWidth()*bitmap.getHeight(); j+=bitmap.getWidth()){
+            count++;
+            left+=intArray[j];
+        }
+        if (count!=0)
+            left= (left/count);
+        if (left<0)
+        left= left*-1;
+        count=0;
+
+        for(int j=bitmap.getWidth(); j<bitmap.getWidth()*bitmap.getHeight(); j+=bitmap.getWidth()){
+            count++;
+            right+=intArray[j];
+        }
+        if (count!=0)
+        right= (right/count);
+
+        if (right<0)
+            right= right*-1;
+
+        for (int i =0; i<intArray.length; i++){
+
+                 if (x==bitmap1.getWidth()-1){
+                 x=0;
+                 if (y!=bitmap.getHeight()-1){
+                 y++;}
+                 }else{
+            x++;}
+
+            if (intArray[i]<0)
+                w= intArray[i]*-1;
+            else
+                w=intArray[i];
+
+            if (w/top>1.75||top/w>1.75&&w/bottom>1.75||bottom/w>1.75&&w/left>1.75||left/w>1.75&&right/w>1.75||w/right>1.75){
+                bitmap1.setPixel(x, y, intArray[i]);
+            }
+        }
+        imageView.setImageBitmap(bitmap1);
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+        switch(requestCode) {
+            case SELECT_PHOTO:
+                if(resultCode == RESULT_OK){
+                    try {
+                        final Uri imageUri = imageReturnedIntent.getData();
+                        final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                        final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                         Bitmap resizedBitmap= Bitmap.createScaledBitmap(selectedImage, (int) (selectedImage.getWidth() * .1), (int) (selectedImage.getHeight() * .1), false);
+                        backgroundColor(resizedBitmap);
+
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+        }
     }
 
     @Override
